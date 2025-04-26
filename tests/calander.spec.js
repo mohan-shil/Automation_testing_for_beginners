9b4eab4643412959b6ad9dff1d143907fa230728
mohanshil <mohanshil05@gmail.com>
2025-04-26 17:34:29 +0200

    calander section test case added

--- tests/calander.spec.js
+++ tests/calander.spec.js
@@ -7,6 +7,7 @@ test('Calendar Validations', async ({page})=>
     const monthName = "June";
     const date = "15";
     const year = "2027";
+    const expectedList = [monthName, date, year];
 
     await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers');
     await page.locator('.react-date-picker__button').nth(1).click();
@@ -16,5 +17,13 @@ test('Calendar Validations', async ({page})=>
     await page.getByText(monthName).click();
     await page.locator(".react-calendar__month-view__days__day").getByText(date).click();
     
+    //assertion of value 
+    const inputs = await page.locator('.react-date-picker__inputGroup input');
+    for ( let index = 0; index < inputs.length; index++)
+    {
+        const value = input[index].getAttribute('value');
+        expect(value).toEqual(expectedList[index]);
+        
+    }
 
 });
